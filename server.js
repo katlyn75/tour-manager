7a930d28fac0a897ba7422cc9c321eca29632dee
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-18 18:01:59 -0700

    changes to lab file and weird server.js bug

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { connect } from 'tls';
+/*eslint no-console: off */
 
 const http = require('http');
 const app = require('./lib/app');
@@ -6,13 +6,10 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 3000;
 
-//used my heroku when adding an mLab to instance
-// this env name "MONGODB_URI" is used by heroku when adding an mLab instance
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pirates';
+//env name used by heroku when adding an mLab to instance
+//const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pirates';
 
-connect(MONGODB_URI);
-
-
+//connect(MONGODB_URI);
 
 const server = http.createServer(app);
 
