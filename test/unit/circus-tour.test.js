3f232a3300095a8206be9feecfd71cf4bd3cf494
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-19 16:09:59 -0700

    still adjusting

--- test/unit/circus-tour.test.js
+++ test/unit/circus-tour.test.js
@@ -1,26 +1,28 @@
 
 const { assert } = require('chai');
-const CircusTour = require('../../lib/models/circusTour');
+const circusTour = require('../../lib/models/circusTour');
+
 
 describe('Circus Tour', () => {
 
+    let data = {
+        title: 'Portland Magical Tour',
+        activities: ['Bearded Lady', 'Sword Swallower', 'Fire Breather'],
+        launchDate: new Date(),
+        },
+        stops: []
+
+    };
 
     it('valid model check', () => {
-        const data = {
-            title: 'Portland Magical Tour',
-            activities: ['Bearded Lady', 'Sword Swallower', 'Fire Breather'],
-            launchDate: new Date(),
-            stops: [],
-                
-        };
+        
+    const circusTour = new CircusTour(data);
 
-        const circusTour = new CircusTour(data);
-        data._id = circusTour._id;
-        data.stops[0]._id = circusTour.stops[0]._id;
+    circusTour.data._id = CircusTour._id;
+    data.stops[0]._id = circusTour.stops[0]._id;
 
-        assert.deepEqual(circusTour.toJSON(), data);
+    assert.deepEqual(circusTour.toJSON(), data);
 
-    });
+});
 
     
-});
